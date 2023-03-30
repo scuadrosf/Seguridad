4971a85b7d4c627119f2edf027aeac0cb1db852a
AdriPdr <102820600+AdriPdr@users.noreply.github.com>
2023-03-30 14:45:51 +0200

    Ej1

---
+++ Codigos/Cesar.py
@@ -0,0 +1,10 @@
+def cesar(cifrado, desplazamiento):
+    descifrado = []
+    for i in cifrado:
+        descifrado.append() =
+
+
+if __name__ == '__main__':
+    cifrado = str(input())
+    for i in range(26):
+        descifrado = cesar(cifrado, desplazamiento)
