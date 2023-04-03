bb3f86cf0a6efe424a6264326356282d37f1c86c
AdriPdr <102820600+AdriPdr@users.noreply.github.com>
2023-04-03 14:02:15 +0200

    Update Base64.py

--- Base64.py
+++ Base64.py
@@ -4,16 +4,28 @@ import base64
 def codificar(en_claro):
     binario = en_claro.encode("utf-8")
     codificado = base64.b64encode(binario).decode("utf-8")
-    print("Codificado:", codificado)
+    print("Cifrado:", codificado)
 
 
 def decodificar(cifrado):
     decodificado = base64.b64decode(cifrado).decode("utf-8")
-    print("Decodificado:", decodificado)
+    print("Descifrado:", decodificado)
     return decodificado
 
 
 if __name__ == '__main__':
-    cifrado = str(input())
-    decodificado = decodificar(cifrado)
-    codificar(decodificado)
+    print("1.Cifrar")
+    print("2.Descifrar")
+    print("3.Los 2")
+    op = int(input())
+    if op==1:
+        enClaro = str(input())
+        codificar(enClaro)
+    elif op==2:
+        codificado = str(input())
+        decodificar(codificado)
+    else:
+        codificado = str(input())
+        enClaro = decodificar(codificado)
+        codificar(enClaro)
+
