898c02db8c5ec05ca6141f1aba4d377f5d90a7cc
scuadrosf <101071401+scuadrosf@users.noreply.github.com>
2023-04-20 10:38:27 +0200

    update vigenere

--- Vigenere.py
+++ Vigenere.py
@@ -1,36 +1,45 @@
-def vigenere_cifrado(mensaje, clave, alfabeto):
+def cifrar(mensaje, clave, alfabeto):
     cifrado = ""
     n = len(alfabeto)
     for i in range(len(mensaje)):
         if mensaje[i] in alfabeto:
             x = alfabeto.index(mensaje[i])
-            y = alfabeto.index(clave[i % len(clave)].lower())
+            y = alfabeto.index(clave[i % len(clave)])
             if clave[i % len(clave)].isupper():
                 cifrado += alfabeto[(x + y) % n].upper()
             else:
                 cifrado += alfabeto[(x + y) % n]
         else:
             cifrado += mensaje[i]
-    return cifrado
+    print("Mensaje cifrado:", cifrado)
 
-def vigenere_descifrado(cifrado, clave, alfabeto):
+def descifrar(cifrado, clave, alfabeto):
     mensaje = ""
     n = len(alfabeto)
     for i in range(len(cifrado)):
         if cifrado[i] in alfabeto:
             x = alfabeto.index(cifrado[i])
-            y = alfabeto.index(clave[i % len(clave)].lower())
+            y = alfabeto.index(clave[i % len(clave)])
             if clave[i % len(clave)].isupper():
                 mensaje += alfabeto[(x - y) % n].upper()
             else:
                 mensaje += alfabeto[(x - y) % n]
         else:
             mensaje += cifrado[i]
-    return mensaje
+    print("Mensaje descrifrado:", mensaje)
 
-alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-mensaje_cifrado = "QqmiaiiiYmisqmwmxijs"
-clave = "Vigenere"
 
-mensaje_descifrado = vigenere_descifrado(mensaje_cifrado, clave, alfabeto)
-print("Mensaje descifrado:", mensaje_descifrado)
+if __name__ == '__main__':
+    alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    #mensaje_cifrado = "QqmiaiiiYmisqmwmxijs"
+    clave = "Vigenere"
+
+    print("1.Cifrar")
+    print("2.Descifrar")
+    op = int(input())
+    if op == 1:
+        enClaro = str(input())
+        cifrar(enClaro, clave, alfabeto)
+    elif op == 2:
+        codificado = str(input())
+        descifrar(codificado, clave, alfabeto)
